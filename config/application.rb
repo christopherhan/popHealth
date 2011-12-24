887860b0415796e2b14e2ea1aeb2372c652d44df
Chris Han <christopher.t.han@gmail.com>
2011-12-23 23:15:22 -0600

    i18n index and top nav

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,7 @@ module PopHealth
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
@@ -48,7 +49,6 @@ module PopHealth
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
     # add devise views
 #    config.paths["app/views/devise"]
     config.paths["app/views"] << "app/views/devise"
