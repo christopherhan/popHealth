2f24c97cf53b67944cc5c28b4c38892140c78073
Chris Han <christopher.t.han@gmail.com>
2011-12-25 00:18:09 -0600

    Dashboard controller

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,13 @@
+class DashboardController < ApplicationController
+  
+  
+  skip_authorization_check
+
+  def index
+    @num_males = Record.where(:gender => 'M').to_a.length
+    @num_females = Record.where(:gender => 'F').to_a.length
+    @ages, @age_groups = Record.get_ages
+
+  end
+  
+end
